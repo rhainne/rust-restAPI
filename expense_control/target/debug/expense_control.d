bbf4f3ce045060362a30ba897826d67f78db30ea
adri <adriangg@uoc.edu>
2023-02-18 18:05:54 +0100

    initial commit

---
+++ expense_control/target/debug/expense_control.d
@@ -0,0 +1,1 @@
+D:\Rust\rust_tutorial\expense_control_api\rust-restAPI\expense_control\target\debug\expense_control.exe: D:\Rust\rust_tutorial\expense_control_api\rust-restAPI\expense_control\src\main.rs
